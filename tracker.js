f1b6495428510dc747f30e889e3ab938d8484298
nithiyagunashekhar <nithiya.g@thewholewave.com>
2025-03-05 09:39:43 +0530

    API updated

--- tracker.js
+++ tracker.js
@@ -1,22 +1,12 @@
 (function () {
-  const API_ENDPOINT = "https://b6cf-60-243-64-58.ngrok-free.app/webhook";
+  const API_ENDPOINT = "https://012c-60-243-64-58.ngrok-free.app/webhook";
   let sessionStartTime = Date.now();
 
   // Function to Get or Generate User ID
   function getUserId() {
-    let userId = localStorage.getItem("userId"); 
-    let guestId = localStorage.getItem("guestId"); 
-
-    if (userId) {
-        return { userId, guestId: null }; 
-    } else {
-        if (!guestId) {
-            guestId = "guest_" + Math.random().toString(36).substr(2, 9); 
-            localStorage.setItem("guestId", guestId);
-        }
-        return { userId: null, guestId }; 
-    }
+    return localStorage.getItem("userId") || null; // Fetch the actual user ID
 }
+
 
 
   function getUserEmail() {
@@ -44,7 +34,7 @@ function getUserRole() {
 
   // Send Tracking Data
   function sendTrackingData(eventType, extraData = {}) {
-      const {userId, guestId} = getUserId();
+      const userId = getUserId();
       const email = getUserEmail();
       const role = getUserRole();
       const userAgent = navigator.userAgent;
@@ -54,7 +44,6 @@ function getUserRole() {
 
       const trackingData = {
           userId,
-          guestId,
           email,
           role,
           eventType,
