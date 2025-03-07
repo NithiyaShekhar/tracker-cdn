cccefd24cbfc805a4cceb16351a79d49551e4cf3
nithiyagunashekhar <nithiya.g@thewholewave.com>
2025-03-07 10:05:23 +0530

    API URL updated

--- tracker.js
+++ tracker.js
@@ -1,5 +1,5 @@
 (function () {
-    const API_ENDPOINT = "https://012c-60-243-64-58.ngrok-free.app/webhook";
+    const API_ENDPOINT = "https://3f35-60-243-64-58.ngrok-free.app/webhook";
     let sessionStartTime = Date.now();
   
     // Function to Get or Generate User ID
